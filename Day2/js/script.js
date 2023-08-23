6f3e3f06812ce6e918f443a246c77a5375d0545a
Iosu Gómez <131406997+IosuVitoria@users.noreply.github.com>
2023-08-23 17:21:32 +0200

    Day 2 - Fibonacci Sequence

---
+++ Day2/js/script.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('fibonacci-form');
+const sequenceOutput = document.getElementById('sequence-output');
+
+function generateFibonacciSequence(n) {
+  const sequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
+  return sequence;
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const numberInput = document.getElementById('number-input');
+  const n = parseInt(numberInput.value);
+
+  if (isNaN(n) || n <= 0) {
+    alert('Por favor, ingresa un número positivo válido.');
+    return;
+  }
+
+  const fibonacciSequence = generateFibonacciSequence(n);
+
+  let output = '<p>Secuencia Fibonacci:</p><ul>';
+  fibonacciSequence.forEach(num => {
+    output += `<li>${num}</li>`;
+  });
+  output += '</ul>';
+
+  sequenceOutput.innerHTML = output;
+});
