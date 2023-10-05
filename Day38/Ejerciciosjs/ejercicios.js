b070963662d99b70f6d029c2cf3ac60358381791
Iosu Gómez <131406997+IosuVitoria@users.noreply.github.com>
2023-10-05 22:52:16 +0200

    Tutorial de Tailwind

---
+++ Day38/Ejerciciosjs/ejercicios.js
@@ -0,0 +1,28 @@
+//Nivel fácil. 
+
+//Ejercicio 1: Imprimir "Hola, mundo!" en la consola por medio de una función.
+
+const saluda = () =>{
+    console.log("Hola mundo!");
+}
+
+console.log("Ejercicio 1 - Nivel fácil");
+saluda();
+
+// Calcular la suma de dos números.
+
+const suma = (a, b) => {
+    let resultado = a+b;
+    return resultado;
+}
+
+console.log("Ejercicio 2 - Nivel fácil");
+console.log(suma(3,4));
+// Calcular el producto de dos números.
+// Calcular el área de un triángulo.
+// Calcular el área de un círculo.
+// Convertir grados Celsius a Fahrenheit.
+// Encontrar el mayor de dos números.
+// Verificar si un número es par o impar.
+// Imprimir los números del 1 al 10.
+// Imprimir los números pares del 1 al 20.
---
+++ Day38/Ejerciciosjs/enunciados.md
@@ -0,0 +1,12 @@
+Nivel Fácil:
+
+Imprimir "Hola, mundo!" en la consola.
+Calcular la suma de dos números.
+Calcular el producto de dos números.
+Calcular el área de un triángulo.
+Calcular el área de un círculo.
+Convertir grados Celsius a Fahrenheit.
+Encontrar el mayor de dos números.
+Verificar si un número es par o impar.
+Imprimir los números del 1 al 10.
+Imprimir los números pares del 1 al 20.
