ff179591cdc258faf0883f242a8e242f1fbf4540
Iosu Gómez <131406997+IosuVitoria@users.noreply.github.com>
2023-10-20 13:08:07 +0200

    Correcciones para ejercicios java

--- Day46/desafiosjavacodewars/8kyu/ejercicio1.java
+++ Day46/desafiosjavacodewars/8kyu/ejercicio1.java
@@ -2,17 +2,13 @@
 
 public class ejercicio1{
 
-    // Método que toma un valor booleano y devuelve una cadena "Sí" para true o "No" para false.
     public static String boolToWord(boolean b) {
-        // Usamos el operador ternario para simplificar la lógica.
-        // Si 'b' es verdadero, devuelve "Sí"; de lo contrario, devuelve "No".
         return b ? "Sí" : "No";
     }
 
     public static void main(String[] args) {
-        // Ejemplo de uso del método boolToWord.
-        boolean valor = true; // Cambia el valor de 'valor' según tus necesidades.
-        String resultado = boolToWord(valor); // Llamamos al método y almacenamos el resultado.
-        System.out.println(resultado); // Imprimimos el resultado en la consola.
+        boolean valor = true; 
+        String resultado = boolToWord(valor); 
+        System.out.println(resultado); 
     }
 }
--- Day46/desafiosjavacodewars/8kyu/ejercicio3.java
+++ Day46/desafiosjavacodewars/8kyu/ejercicio3.java
@@ -1,5 +1,4 @@
 // Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
-
 // The output should be two capital letters with a dot separating them.
 
 
@@ -19,7 +18,7 @@ public class ejercicio3 {
 
             return conversion;
         } else {
-            // En caso de que no se cumpla el formato esperado, puedes manejarlo según tus necesidades.
+            // En caso de que no se cumpla el formato esperado, podemos manejar la excepción.
             return "Formato incorrecto";
         }
     }
